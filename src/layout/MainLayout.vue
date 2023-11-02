21c1837d471c3bbaabd35218269defd6a5b45442
M-app <xlopez3000@gmail.com>
2023-11-02 14:32:21 -0600

    responsive

--- src/layout/MainLayout.vue
+++ src/layout/MainLayout.vue
@@ -4,20 +4,26 @@
       <span class="text-4xl">Carrito</span>
       <Cart/>
     </Sidebar>
+    <Sidebar v-model:visible="mostrarSidebarMobile" position="left" style="width: 400px">
+      <MSidebar/>
+    </Sidebar>
     <Toolbar
         class="toolbar">
       <template #start>
-        start
+        <Button class="mobile-sidebar" icon="pi pi-arrow-left" @click="mostrarSidebarMobile = !mostrarSidebarMobile" :badge="cartProductos.length"></Button>
       </template>
       <template #center>
-       // h
+        <div class="flex align-items-center">
+          <img class="mr-4" src="../assets/logo.png" alt="logo">
+          <span class="text-4xl">Marco's Store</span>
+        </div>
       </template>
       <template #end>
         <Button icon="pi pi-shopping-cart" @click="mostrarCarrito = !mostrarCarrito" :badge="cartProductos.length" />
       </template>
     </Toolbar>
     <div class="main_content">
-      <Categorias/>
+      <MSidebar class="desktop-sidebar"/>
       <div class="content">
         <router-view/>
       </div>
@@ -28,13 +34,28 @@
 <script lang="ts" setup>
 import { ref } from 'vue';
 import {useCart} from "../hooks/useCart";
-import Categorias from "../components/MSidebar.vue";
+import MSidebar from "../components/MSidebar.vue";
 import Cart from "../components/Cart.vue"
 const { cartProductos } = useCart();
 const mostrarCarrito = ref(false);
+const mostrarSidebarMobile = ref(false);
 </script>
 
 <style scoped>
+.mobile-sidebar {
+  display: none;
+}
+.desktop-sidebar {
+  display: block !important;
+}
+@media screen and (max-width: 520px) {
+   .desktop-sidebar {
+     display: none !important;
+   }
+  .mobile-sidebar {
+    display: block;
+  }
+}
 .toolbar {
   background-color: white;
 }
